d4221fec7727dca5bc743e9732482680877716e1
Gleb <igi2000@inbox.ru>
2020-03-17 10:31:35 +0300

    feat: add login layout and init EditTexts and button in LoginActivity

--- app/src/main/java/com/bsuir/rppba/ui/login/LoginActivity.java
+++ app/src/main/java/com/bsuir/rppba/ui/login/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.bsuir.rppba.ui.login;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,12 +12,21 @@ import com.bsuir.rppba.R;
 public class LoginActivity extends AppCompatActivity implements LoginContract.LoginView {
 
     private LoginPresenter presenter = new LoginPresenter();
+    private EditText loginEditText;
+    private EditText passwordEditText;
+    private Button singInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         presenter.attachView(this);
+
+        loginEditText = findViewById(R.id.login_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        singInButton = findViewById(R.id.sign_in_button);
+
+        buttonOnClick();
     }
 
     @Override
@@ -35,6 +47,15 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Lo
     @Override
     public void showLoadingIndicator(boolean value) {
 
+    }
+
+    private void buttonOnClick (){
+        singInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO authorization
+            }
+        });
     }
 
 }

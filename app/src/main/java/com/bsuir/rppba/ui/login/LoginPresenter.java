c13336fa4bbbcf7083828a014cf859792598a443
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-03-18 21:03:16 +0300

    Minor refactoring

--- app/src/main/java/com/bsuir/rppba/ui/login/LoginPresenter.java
+++ app/src/main/java/com/bsuir/rppba/ui/login/LoginPresenter.java
@@ -1,11 +1,21 @@
 package com.bsuir.rppba.ui.login;
 
+import android.util.Log;
+
 import com.bsuir.rppba.ui.base.BasePresenter;
 
 public class LoginPresenter extends BasePresenter<LoginContract.LoginView> implements LoginContract.LoginPresenter {
 
+    private final String TAG = this.getClass().getSimpleName();
+
     @Override
     public void logInUser(String login, String password) {
+        Log.i(TAG, "Login " + login);
+        Log.i(TAG, "Password " + password);
+
+        //TODO implement API call
+
+        view.onLoginSuccess();
 
     }
 

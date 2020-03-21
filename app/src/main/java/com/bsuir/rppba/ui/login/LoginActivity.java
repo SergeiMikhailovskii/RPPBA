fa1ec52b90f499048e68c6573cddc3b6931162f7
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-03-21 22:37:56 +0300

    Added bottom navigation

--- app/src/main/java/com/bsuir/rppba/ui/login/LoginActivity.java
+++ app/src/main/java/com/bsuir/rppba/ui/login/LoginActivity.java
@@ -1,8 +1,7 @@
 package com.bsuir.rppba.ui.login;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -10,6 +9,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bsuir.rppba.R;
+import com.bsuir.rppba.ui.main.MainActivity;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity implements LoginContract.LoginView {
 
@@ -22,7 +24,7 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Lo
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         presenter.attachView(this);
 
         loginEditText = findViewById(R.id.login_edit_text);
@@ -40,6 +42,8 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Lo
     @Override
     public void onLoginSuccess() {
         Toast.makeText(this, "Login success", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     @Override

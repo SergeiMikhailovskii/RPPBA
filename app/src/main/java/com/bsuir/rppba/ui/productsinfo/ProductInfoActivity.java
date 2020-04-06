257370a3f0c43043feecb251c3ee067fb815b4b1
Gleb <igi2000@inbox.ru>
2020-04-06 11:35:43 +0300

    feat: add new activity_product_info and product_info_list layout

---
+++ app/src/main/java/com/bsuir/rppba/ui/productsinfo/ProductInfoActivity.java
@@ -0,0 +1,16 @@
+package com.bsuir.rppba.ui.productsinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bsuir.rppba.R;
+
+public class ProductInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_info);
+    }
+}
